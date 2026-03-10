c62903cbbdec3b3339e6d4ef4b2f2f727b13d224
nhaar <38634785+nhaar@users.noreply.github.com>
2026-03-10 15:58:55 -0300

    add logic for game in normal order

--- main.cpp
+++ main.cpp
@@ -13,8 +13,13 @@ enum Tile {
     WATER,
     GOAL,
     LOCK,
-    HOLE
+    HOLE,
+    TELEPORTER
 };
+
+bool is_tile_moveable(Tile tile) {
+    return !(tile == Tile::WALL || tile == Tile::WATER || tile == Tile::LOCK);
+}
 
 enum Direction {
     UP,
@@ -23,27 +28,71 @@ enum Direction {
     LEFT
 };
 
+class Coord {
+private:
+    int _x;
+    int _y;
+
+public:
+    Coord(): _x{0}, _y{0} {}
+    Coord(int x, int y): _x{x}, _y{y} {}
+
+    bool is_equal(Coord& other) {
+        return _x == other.get_x() && _y == other.get_y();
+    }
+
+    int get_x() {
+        return _x;
+    }
+
+    int get_y() {
+        return _y;
+    }
+
+    void set(int x, int y) {
+        _x = x;
+        _y = y;
+    }
+
+    void advance_in_direction(Direction direction) {
+        switch (direction) {
+            case Direction::UP:
+                _y -= 1;
+                break;
+            case Direction::DOWN:
+                _y += 1;
+                break;
+            case Direction::LEFT:
+                _x -= 1;
+                break;
+            case Direction::RIGHT:
+                _x += 1;
+                break;
+        }
+    }
+};
+
 class LevelMap {
 private:
     int _width;
     int _height;
     bool _has_key;
     bool _has_block;
-    bool _has_teleported;
-    std::array<int, 2> _key_pos;
-    std::array<int, 2> _block_pos;
-    std::array<int, 2> _puffle_pos;
+    bool _has_teleported = false;
+    bool _picked_key = false;
+    Coord _key_pos;
+    Coord _block_pos;
+    Coord _puffle_pos;
     std::vector<Tile> _tiles;
     int get_index(int x, int y) {
         return _width * y + x;
     }
 public:
-    LevelMap(int width, int height, bool has_key, bool has_block, bool has_teleported, std::array<int, 2> key_pos, std::array<int, 2> block_pos, std::array<int, 2> puffle_pos, std::vector<Tile> tiles) :
+    LevelMap(int width, int height, bool has_key, bool has_block, Coord key_pos, Coord block_pos, Coord puffle_pos, std::vector<Tile> tiles) :
         _width{width},
         _height{height},
         _has_key{has_key},
         _has_block{has_block},
-        _has_teleported{has_teleported},
         _key_pos{key_pos},
         _block_pos{block_pos},
         _puffle_pos{puffle_pos},
@@ -51,6 +100,24 @@ public:
 
     Tile get_tile(int x, int y) {
         return _tiles.at(get_index(x, y));
+    }
+
+    std::vector<Tile> get_tiles() {
+        return _tiles;
+    }
+
+    void melt_tile(Coord new_pos) {
+        Tile tile = get_tile(_puffle_pos.get_x(), _puffle_pos.get_y());
+        if (tile == Tile::THICK) {
+            change_tile(_puffle_pos.get_x(), _puffle_pos.get_y(), Tile::ICE);
+        } else if (tile == Tile::ICE) {
+            change_tile(_puffle_pos.get_x(), _puffle_pos.get_y(), Tile::WATER);
+        }
+        _puffle_pos = new_pos;
+    }
+
+    void remove_lock(Coord pos) {
+        change_tile(pos.get_x(), pos.get_y(), Tile::ICE);
     }
 
     void change_tile(int x, int y, Tile tile) {
@@ -65,12 +132,61 @@ public:
         return _height;
     }
 
-    std::array<int, 2> get_puffle_pos() {
+    Coord get_puffle_pos() {
         return _puffle_pos;
     }
 
+    bool block_exists() {
+        return _has_block;
+    }
+
+    Coord get_block_pos() {
+        return _block_pos;
+    }
+
     void set_puffle_pos(int x, int y) {
-        _puffle_pos = std::array<int, 2> { x, y };
+        _puffle_pos.set(x, y);
+    }
+
+    void set_block_pos(Coord pos) {
+        _block_pos = pos;
+    }
+
+    bool has_teleported() {
+        return _has_teleported;
+    }
+
+    Coord find_other_portal(Coord this_portal) {
+        for (int y = 0; y < _height; y++) {
+            for (int x = 0; x < _width; x++) {
+                Tile tile = get_tile(x, y);
+                Coord coord = Coord(x, y);
+                if (tile == Tile::TELEPORTER && !this_portal.is_equal(coord)) {
+                    return coord;
+                }
+            }
+        }
+        throw std::runtime_error("Could not find other portal");
+    }
+
+    Coord get_key_pos() {
+        return _key_pos;
+    }
+
+    bool has_key() {
+        return _has_key;
+    }
+
+    bool picked_key() {
+        return _picked_key;
+    }
+
+    void pick_key() {
+        _picked_key = true;
+    }
+
+    void use_key() {
+        _picked_key = false;
     }
 };
 
@@ -83,9 +199,9 @@ private:
     bool _key_set = false;
     bool _block_set = false;
     bool _tiles_set = false;
-    std::array<int, 2> _spawn;
-    std::array<int, 2> _key;
-    std::array<int, 2> _block;
+    Coord _spawn;
+    Coord _key;
+    Coord _block;
     std::vector<Tile> _tiles;
     std::string _data;
 
@@ -98,7 +214,7 @@ public:
 
     }
 
-    std::array<int, 2> parse_coord(std::string data) {
+    Coord parse_coord(std::string data) {
         std::string x = "";
         std::string y = "";
         bool x_done = false;
@@ -113,7 +229,7 @@ public:
                 }
             }
         }
-        return std::array<int, 2> { std::stoi(x), std::stoi(y) };
+        return Coord(std::stoi(x), std::stoi(y));
     }
 
     std::vector<Tile> parse_tiles(std::string data) {
@@ -138,6 +254,9 @@ public:
                     break;
                 case 'H':
                     tile = Tile::HOLE;
+                    break;
+                case 'P':
+                    tile = Tile::TELEPORTER;
                     break;
                 default:
                     throw std::runtime_error("Invalid tile type: " + c);
@@ -206,7 +325,6 @@ public:
             _height,
             _key_set,
             _block_set,
-            false,
             _key,
             _block,
             _spawn,
@@ -219,9 +337,54 @@ private:
     LevelMap _level;
 public:
     bool advance(Direction direction) {
-        return true;
+        Coord pos = _level.get_puffle_pos();
+        pos.advance_in_direction(direction);
+        // check for block
+        if (_level.block_exists() && (_level.get_block_pos()).is_equal(pos)) {
+            Coord new_block_pos = _level.get_block_pos();
+            Coord final_block_pos = new_block_pos;
+            new_block_pos.advance_in_direction(direction);
+            Tile block_tile = _level.get_tile(new_block_pos.get_x(), new_block_pos.get_y());
+            bool moved = false;
+            while (is_tile_moveable(block_tile)) {
+                final_block_pos = new_block_pos;
+                moved = true;
+                new_block_pos.advance_in_direction(direction);
+                block_tile = _level.get_tile(new_block_pos.get_x(), new_block_pos.get_y());
+            }
+            if (moved) {
+                _level.set_block_pos(final_block_pos);
+                _level.melt_tile(pos);
+            }
+            return true;
+        } else {
+            Tile tile = _level.get_tile(pos.get_x(), pos.get_y());
+            if (is_tile_moveable(tile)) {
+                _level.melt_tile(pos);
+                if (tile == Tile::TELEPORTER && !_level.has_teleported()) {
+                    Coord destination = _level.find_other_portal(pos);
+                    _level.set_puffle_pos(destination.get_x(), destination.get_y());
+                }
+                if (_level.has_key() && !_level.picked_key() && _level.get_key_pos().is_equal(pos)) {
+                    _level.pick_key();
+                }
+                return true;
+            } else if (tile == Tile::LOCK && _level.picked_key()) {
+                _level.use_key();
+                _level.remove_lock(pos);
+                _level.melt_tile(pos);
+                return true;
+            }
+        }
+
+        return false;
     }
     bool is_solved() {
+        for (Tile tile : _level.get_tiles()) {
+            if (tile == Tile::ICE || tile == Tile::THICK || tile == Tile::LOCK) {
+                return false;
+            }
+        }
         return true;
     }
     Game(LevelMap map) : _level{map} {
@@ -237,9 +400,9 @@ private:
     
 public:
     bool is_in_spawn() {
-        std::array<int, 2> spawn = _original_level->get_puffle_pos();
-        std::array<int, 2> pos = _level.get_puffle_pos();
-        return spawn.at(0) && pos.at(0);
+        Coord spawn = _original_level->get_puffle_pos();
+        Coord pos = _level.get_puffle_pos();
+        return pos.is_equal(spawn);
     }
 
     void undo_water_tile(int x, int y) {
@@ -278,9 +441,9 @@ public:
     }
 
     std::vector<BackwardsGame> get_possible_backward_variations() {
-        std::array<int, 2> pos = _level.get_puffle_pos();
-        int x = pos.at(0);
-        int y = pos.at(1);
+        Coord pos = _level.get_puffle_pos();
+        int x = pos.get_x();
+        int y = pos.get_y();
         std::vector<BackwardsGame> games = {};
         try_add_new_game(games, x, y - 1, Direction::DOWN);
         try_add_new_game(games, x, y + 1, Direction::UP);
@@ -347,6 +510,7 @@ int main(int argc, char* argv[]) {
     // step 4: check which of the solutions are valid
     std::vector<std::vector<Direction>> valid_solutions = {};
     for (std::vector<Direction> solution : found_solutions) {
+        std::reverse(solution.begin(), solution.end());
         Game game_simulation = Game(initial_position);
         bool valid = true;
         for (Direction direction : solution) {
@@ -355,7 +519,7 @@ int main(int argc, char* argv[]) {
                 break;
             }
         }
-        if (game_simulation.is_solved()) {
+        if (valid && game_simulation.is_solved()) {
             valid_solutions.push_back(solution);
         }
     }
@@ -365,7 +529,6 @@ int main(int argc, char* argv[]) {
     for (std::vector<Direction> solution : valid_solutions) {
         i += 1;
         out << "Solution #" << i << ": ";
-        std::reverse(solution.begin(), solution.end());
         for (Direction direction : solution) {
             std::string name;
             switch (direction) {
