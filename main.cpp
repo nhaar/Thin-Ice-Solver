07808e5c8053630c1875c840060eb7fdc2bfa384
nhaar <38634785+nhaar@users.noreply.github.com>
2026-03-10 12:55:00 -0300

    add data parser

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <array>
 #include <algorithm>
+#include <stdexcept>
 
 enum Tile {
     ICE,
@@ -45,10 +46,6 @@ public:
         _puffle_pos{puffle_pos},
         _tiles{tiles} {}
 
-    LevelMap(std::string data) {
-
-    }
-
     LevelMap clone() {
         std::array<int, 2> new_key = _key_pos;
         std::array<int, 2> new_block = _block_pos;
@@ -68,6 +65,146 @@ public:
     }
 };
 
+class DataParser {
+private:
+    int _index = 0;
+    int _width = 0;
+    int _height = 0;
+    bool _spawn_set = false;
+    bool _key_set = false;
+    bool _block_set = false;
+    bool _tiles_set = false;
+    std::array<int, 2> _spawn;
+    std::array<int, 2> _key;
+    std::array<int, 2> _block;
+    std::vector<Tile> _tiles;
+    std::string _data;
+
+    char get_char() {
+        return _data[_index];
+    }
+
+public:
+    DataParser(std::string data) : _data{data} {
+
+    }
+
+    std::array<int, 2> parse_coord(std::string data) {
+        std::string x = "";
+        std::string y = "";
+        bool x_done = false;
+        for (char c : data) {
+            if (c == ',') {
+                x_done = true;
+            } else {
+                if (x_done) {
+                    y += c;
+                } else {
+                    x += c;
+                }
+            }
+        }
+        return std::array<int, 2> { std::stoi(x), std::stoi(y) };
+    }
+
+    std::vector<Tile> parse_tiles(std::string data) {
+        std::vector<Tile> tiles = {};
+        for (char c : data) {
+            Tile tile;
+            switch (c) {
+                case 'I':
+                    tile = Tile::ICE;
+                    break;
+                case 'W':
+                    tile = Tile::WALL;
+                    break;
+                case 'T':
+                    tile = Tile::THICK;
+                    break;
+                case 'G':
+                    tile = Tile::GOAL;
+                    break;
+                case 'L':
+                    tile = Tile::LOCK;
+                    break;
+                case 'H':
+                    tile = Tile::HOLE;
+                    break;
+                default:
+                    throw std::runtime_error("Invalid tile type: " + c);
+            }
+            tiles.push_back(tile);
+        }
+        
+        return tiles;
+    }
+
+    void parse_line() {
+        bool key_done = false;
+        std::string key = "";
+        std::string value = "";
+        while (_index < _data.length()) {
+            char c = _data[_index];
+            if (c == '=') {
+                key_done = true;
+            } else if (c == '\n') {
+                _index++;
+                break;
+            } else {
+                if (key_done) {
+                    value += c;
+                } else {
+                    key += c;
+                }
+            }
+            _index++;
+        }
+
+        if (key == "width") {
+            _width = std::stoi(value);
+        } else if (key == "height") {
+            _height = std::stoi(value);
+        } else if (key == "spawn") {
+            _spawn_set = true;
+            _spawn = parse_coord(value);
+        } else if (key == "key") {
+            _key_set = true;
+            _key = parse_coord(value);
+        } else if (key == "block") {
+            _block_set = true;
+            _block = parse_coord(value);
+        } else if (key == "tiles") {
+            _tiles_set = true;
+            _tiles = parse_tiles(value);   
+        }
+    }
+
+    LevelMap parse() {
+        while (_index < _data.length()) {
+            parse_line();
+        }
+        if (!_spawn_set) {
+            throw std::runtime_error("No spawn point in level");
+        }
+        if (!_tiles_set) {
+            throw std::runtime_error("No tiles defined");
+        }
+        if (_width == 0 || _height == 0) {
+            throw std::runtime_error("Map dimensions not set");
+        }
+        return LevelMap(
+            _width,
+            _height,
+            _key_set,
+            _block_set,
+            false,
+            _key,
+            _block,
+            _spawn,
+            _tiles
+        );
+    }
+};
 class Game {
 private:
     LevelMap _level;
@@ -107,8 +244,10 @@ int main(int argc, char* argv[]) {
     std::ifstream file(argv[1]);
     std::string input_file  { std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>() };
 
+    DataParser parser = DataParser(input_file);
+
     // step 2: create the base game that will be unsolved
-    LevelMap initial_position = LevelMap(input_file);
+    LevelMap initial_position = parser.parse();
 
     std::vector<std::vector<Direction>> found_solutions = {};
     std::vector<BackwardsGame> in_progress_games = {
